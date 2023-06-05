9f195a7614be540bad321e349f8a90ebe78eb35e
Mikael Kaandorp <m.kaandorp@fz-juelich.de>
2023-06-05 18:58:01 +0200

    required modules for python

---
+++ load_modules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+module load Stages/2023  
+module load Intel/2022.1.0  ParaStationMPI/5.8.0-1-mt
+module load CDO/2.1.1
+module load GCCcore/.11.3.0
+module load netcdf4-python/1.6.1-serial
+module load xarray/2022.9.0
+module load NCO/5.1.3
+
+
